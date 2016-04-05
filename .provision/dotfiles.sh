de306805628b7558fc616758fb17bc969062723f
Navneet Potti <nav@cs.wisc.edu>
2016-04-04 19:07:20 -0500

    Fix typo

--- .provision/dotfiles.sh
+++ .provision/dotfiles.sh
@@ -3,6 +3,6 @@
 cd $SRC_DIR
 DOTFILES=$MYHOME/dotfiles
 if [ ! -d $DOTFILES ]; then
-	  git clone https://navsan@bitbucket.org/navsan/dotfiles.git 
-    ln -sf $PROVISION_DIR/dotfiles $DOTFILES
+	git clone https://navsan@bitbucket.org/navsan/dotfiles.git 
+	ln -sf $SRC_DIR/dotfiles $DOTFILES
 fi
