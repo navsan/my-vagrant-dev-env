f43babc3997b2c1fdeb38fe5ef8b5f76fd37ba86
Navneet Potti <nav@cs.wisc.edu>
2016-04-05 00:01:06 -0500

    Install pre-requisite clang/llvm libraries

--- rtags.sh
+++ rtags.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-if [ $(which rc | grep -c "not found") -eq 1]; then
-    cd $SRC_DIR
-    git clone https://github.com/Andersbakken/rtags.git
-    cd rtags
-    git submodule init
-    git submodule update
+if ! which rc > /dev/null ; then
+	# Install llvm and clang stuff 
+	# wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+	sudo apt-get install -y clang-3.7 libclang-common-3.7-dev libclang-3.7-dev libclang1-3.7 libllvm3.7 lldb-3.7 llvm-3.7 llvm-3.7-dev llvm-3.7-examples llvm-3.7-runtime clang-modernize-3.7 clang-format-3.7 lldb-3.7-dev
+
+	cd $SRC_DIR
+	if [ ! -d rtags ]; then 
+	    git clone https://github.com/Andersbakken/rtags.git
+    	git submodule init
+    	git submodule update
+	else 
+		cd rtags 
+		git pull 
+	fi
+    cd $SRC_DIR/rtags
     sudo apt-get install -y libncurses5-dev
     mkdir build
     cd build
     cmake ..
     make -j 3
-    make install
+    sudo make install
 fi
