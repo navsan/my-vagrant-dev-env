81cfcfb33d17ef620224b71538f951edd9e3ec45
Navneet Potti <npotti@pivotal.io>
2016-04-04 20:11:12 -0500

    Simplified vim setup because I'm mostly switching to spacemacs for now.

--- vim.sh
+++ vim.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-if [ $(vim --version | grep -c "Compiled by vagrant") -eq 0 ]; then
-	mkdir -p $SRC_DIR
-	cd $SRC_DIR
-	wget http://mirrors-usa.go-parts.com/pub/vim/unix/vim-7.4.tar.bz2
-	tar jxvf vim-7.4.tar.bz2
-	cd $SRC_DIR/vim74
-	PY_CONFIG=`echo "import sysconfig as s; print s.get_config_var('LIBPL')" | python`
-	./configure --enable-cscope --with-features=huge --enable-pythoninterp --with-python-config-dir=$PY_CONFIG --prefix=/usr
-	make -j 4
-	sudo checkinstall -Dy --install --pkgname=vim-7.4
-	sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-	sudo update-alternatives --set editor /usr/bin/vim
-	sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-	sudo update-alternatives --set vi /usr/bin/vim
-fi
+## ----- Skipping compilation of vim from source
+## ----- Because I'm using spacemacs instead and this is needed only for YCM
+# if [ $(vim --version | grep -c "Compiled by vagrant") -eq 0 ]; then
+# 	mkdir -p $SRC_DIR
+# 	cd $SRC_DIR
+# 	wget http://mirrors-usa.go-parts.com/pub/vim/unix/vim-7.4.tar.bz2
+# 	tar jxvf vim-7.4.tar.bz2
+# 	cd $SRC_DIR/vim74
+# 	PY_CONFIG=`echo "import sysconfig as s; print s.get_config_var('LIBPL')" | python`
+# 	./configure --enable-cscope --with-features=huge --enable-pythoninterp --with-python-config-dir=$PY_CONFIG --prefix=/usr
+# 	make -j 4
+# 	sudo checkinstall -Dy --install --pkgname=vim-7.4
+# 	sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+# 	sudo update-alternatives --set editor /usr/bin/vim
+# 	sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+# 	sudo update-alternatives --set vi /usr/bin/vim
+# fi
 
 # Set up Vundle
 if [ ! -d "$MYHOME/.vim/bundle/Vundle.vim" ]; then
@@ -24,13 +26,13 @@ if [ ! -d "$MYHOME/.vim/bundle/Vundle.vim" ]; then
 fi
 
 
-cd $SRC_DIR/vim74
+cd $SRC_DIR
 if [ ! -d "vim-wasabi-colorscheme" ]; then 
 	git clone https://github.com/thomd/vim-wasabi-colorscheme.git 
+	cd vim-wasabi-colorscheme/
+	mkdir -p $MYHOME/.vim/colors
+	cp -v colors/* $MYHOME/.vim/colors
 fi
-cd vim-wasabi-colorscheme/
-mkdir -p $MYHOME/.vim/colors
-cp -v colors/* $MYHOME/.vim/colors
 
 ln -sf $DOTFILES/.vimrc $MYHOME/.vimrc
 # ex -c ":PluginInstall"
@@ -39,14 +41,16 @@ vim -E -s -c "source $MYHOME/.vimrc" -c PluginInstall -c qa -q /vagrant/vim_erro
 # vim +PluginInstall +qall
 # ex -c ":PluginInstall"
 
-if [ ! -d $MYHOME/.vim/bundle/YouCompleteMe ]; then
-	mkdir -p $MYHOME/.vim/bundle
-	cd $MYHOME/.vim/bundle
-	git clone https://github.com/Valloric/YouCompleteMe.git 
-	cd $MYHOME/.vim/bundle/YouCompleteMe
-	git submodule update --init --recursive
-fi
-if [ ! -e "$MYHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/ycm_core.so" ]; then
-	cd $MYHOME/.vim/bundle/YouCompleteMe
-	./install.py --clang-completer
-fi
+## ----- Skipping YouCompleteMe setup for now
+## ----- Because I'm using spacemacs instead and this takes way too long
+# if [ ! -d $MYHOME/.vim/bundle/YouCompleteMe ]; then
+# 	mkdir -p $MYHOME/.vim/bundle
+# 	cd $MYHOME/.vim/bundle
+# 	git clone https://github.com/Valloric/YouCompleteMe.git 
+# 	cd $MYHOME/.vim/bundle/YouCompleteMe
+# 	git submodule update --init --recursive
+# fi
+# if [ ! -e "$MYHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/ycm_core.so" ]; then
+# 	cd $MYHOME/.vim/bundle/YouCompleteMe
+# 	./install.py --clang-completer
+# fi
