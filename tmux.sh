cae0c9ba02f8ad0d9a42db6db539c05bc6b3a5e7
Navneet Potti <navsan@gmail.com>
2016-04-06 02:00:07 -0500

    Use .zshrc from DOTFILES. Don't bother with .myrc.

--- tmux.sh
+++ tmux.sh
@@ -3,5 +3,4 @@
 if [ ! -f $MYHOME/.tmux.conf ]; then
     sudo apt-get install -y tmux
     ln -sf $DOTFILES/.tmux.conf $MYHOME/.tmux.conf 
-    echo "alias tmux=\"tmux -2\"" >> $MYHOME/.myrc  # 256 color support
 fi
--- zsh.sh
+++ zsh.sh
@@ -5,17 +5,10 @@ chsh -s `which zsh` vagrant
 if [ ! -d "$MYHOME/.oh-my-zsh" ]; then
 	  git clone git://github.com/robbyrussell/oh-my-zsh.git $MYHOME/.oh-my-zsh
 fi
-cp $MYHOME/.oh-my-zsh/templates/zshrc.zsh-template $MYHOME/.zshrc
+ln -sf $DOTFILES/.zshrc $MYHOME/.zshrc 
+ln -sf $DOTFILES/nav.zsh-theme $MYHOME/.oh-my-zsh/themes/nav.zsh-theme
 
-# Create a common .myrc to be used by both .bashrc and .zshrc
-if [ ! -e $MYHOME/.myrc ]; then
-	  echo -e "\n\n## ~/.bashrc is marked READ-ONLY. Use ~/.myrc instead." >> $MYHOME/.bashrc
-	  echo "if [ -e ~/.myrc ]; then source ~/.myrc; fi" >> $MYHOME/.bashrc
-	  chmod a-w $MYHOME/.bashrc
-	  echo -e "\n\n## ~/.zshrc is marked READ-ONLY. Use ~/.myrc instead." >> $MYHOME/.zshrc
-	  echo "if [ -e ~/.myrc ]; then source ~/.myrc; fi" >> $MYHOME/.zshrc
-	  chmod a-w $MYHOME/.zshrc
-	  touch $MYHOME/.myrc
-	  chown vagrant $MYHOME/.myrc
-	  chmod u+w $MYHOME/.myrc
-fi
+# Mark .bashrc read-only 
+echo -e "\n\n## ~/.bashrc is marked READ-ONLY. Use ~/.zshrc instead." >> $MYHOME/.bashrc
+chmod a-w $MYHOME/.bashrc
+
