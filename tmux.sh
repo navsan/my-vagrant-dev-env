3af497dc3f16e230a9e0fe203b54e6bbbedf4d24
Navneet Potti <npotti@pivotal.io>
2016-04-09 00:54:27 -0500

    Fixed a typo (can't use ~ for home directory).

--- tmux.sh
+++ tmux.sh
@@ -19,6 +19,6 @@ if [ ! -f $MYHOME/.config/powerline/themes ]; then
 	POWERLINE_ROOT=/usr/local/lib/python2.7/dist-packages/powerline
 	mkdir -p $MYHOME/.config/powerline/themes
 	cp -r $POWERLINE_ROOT/config_files/themes/tmux $MYHOME/.config/powerline/themes/tmux 
-	ln -sf $DOTFILES/tmux-powerline.json ~/.config/powerline/themes/tmux/default.json 
+	ln -sf $DOTFILES/tmux-powerline.json $MYHOME/.config/powerline/themes/tmux/default.json 
 fi 
 
