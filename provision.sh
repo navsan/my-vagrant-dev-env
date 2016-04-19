8108479aaed4758424c2c72dc845d87ff88377ae
Navneet Potti <navsan@gmail.com>
2016-04-19 10:19:34 +0000

    Change the order of provisioning.

--- provision.sh
+++ provision.sh
@@ -18,20 +18,20 @@ source $PROVISION_DIR/dev-prereq.sh
 # Download dotfiles 
 source $PROVISION_DIR/dotfiles.sh
 
+# Install and configure spacemacs
+source $PROVISION_DIR/spacemacs.sh
+
 # Install zsh
 source $PROVISION_DIR/zsh.sh
 
 # Install tmux
 source $PROVISION_DIR/tmux.sh
 
-# Install vim from source
-source $PROVISION_DIR/vim.sh
-
 # Installing rtags
 source $PROVISION_DIR/rtags.sh
 
-# Install and configure spacemacs
-source $PROVISION_DIR/spacemacs.sh
+# Install vim from source
+source $PROVISION_DIR/vim.sh
 
 # Clean up
 sudo apt-get autoremove -y
