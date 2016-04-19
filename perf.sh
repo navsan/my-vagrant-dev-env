7c6d5bc0c3b4e07b75767d5bb50bb3d8bd0c515c
Your NameNavneet Potti <navsan@gamil.comgmail.com>
2016-04-19 06:17:02 +0000

    Fixed the kernel-specific package name

--- perf.sh
+++ perf.sh
@@ -3,7 +3,7 @@
 # Install kernel-specific as well as generic packages. 
 kernel_ver=$(uname -r)
 sudo apt-get install -y linux-tools-generic linux-cloud-tools-generic \
-	linux-tools-generic-$kernel_ver linux-cloud-tools-generic-$kernel_ver
+	linux-tools-$kernel_ver linux-cloud-tools-$kernel_ver
 
 
 # To allow profiling without root privileges
