0441d0691ee4644edc7d576ae6d507281734d7ce
Navneet Potti <navsan@gmail.com>
2016-04-19 23:01:36 +0000

    Added .gitconfig

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,4 +5,5 @@ DOTFILES=$MYHOME/dotfiles
 if [ ! -d $DOTFILES ]; then
 	git clone https://navsan@bitbucket.org/navsan/dotfiles.git 
 	ln -sf $SRC_DIR/dotfiles $DOTFILES
+  ln -sf $DOTFILES/.gitconfig $MYHOME/.gitconfig
 fi
