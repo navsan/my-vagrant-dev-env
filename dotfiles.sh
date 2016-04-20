cca19faaa264c7e8ea6ab28c0efdb143df267839
Navneet Potti <navsan@gmail.com>
2016-04-20 00:41:21 +0000

    Switch to using ssh url for git repo

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,7 +3,8 @@
 cd $SRC_DIR
 DOTFILES=$MYHOME/dotfiles
 if [ ! -d $DOTFILES ]; then
-	git clone https://navsan@bitbucket.org/navsan/dotfiles.git 
-	ln -sf $SRC_DIR/dotfiles $DOTFILES
+	# git clone https://navsan@bitbucket.org/navsan/dotfiles.git 
+  git clone git@bitbucket.org:navsan/dotfiles.git
+  ln -sf $SRC_DIR/dotfiles $DOTFILES
   ln -sf $DOTFILES/.gitconfig $MYHOME/.gitconfig
 fi
